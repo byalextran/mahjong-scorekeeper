f1b61b5f4b2ab3f138a65170f6eb32fc330a479c
Alex Tran <455406+byalextran@users.noreply.github.com>
2025-12-25 18:52:34 -0800

    updated version

--- version.js
+++ version.js
@@ -1,7 +1,13 @@
-const APP_VERSION = '1.0.1';
+const APP_VERSION = '1.0.2';
 
 const CHANGELOG = [
   {
+    version: '1.0.2',
+    changes: [
+      'Entering faans on mobile shows a number pad (instead of the default keyboard).',
+      "It's the little things like this make me happy. 😁"
+    ]
+  }, {
     version: '1.0.1',
     changes: [
       'Added footer with changelog and other links'
