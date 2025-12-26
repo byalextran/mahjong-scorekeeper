1794e91e71f913de0fcb1d1b4ba707a96efd57b3
Alex Tran <455406+byalextran@users.noreply.github.com>
2025-12-25 18:27:27 -0800

    updated version

--- version.js
+++ version.js
@@ -1,10 +1,16 @@
-const APP_VERSION = '1.0.0';
+const APP_VERSION = '1.0.1';
 
 const CHANGELOG = [
-    {
-        version: '1.0.0',
-        changes: [
-            'Initial release.'
-        ]
-    }
+  {
+    version: '1.0.1',
+    changes: [
+      'Added footer with changelog and other links'
+    ]
+  },
+  {
+    version: '1.0.0',
+    changes: [
+      'Initial release'
+    ]
+  }
 ];
