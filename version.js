abc345c2f9fb2dd00710a53fd91f66b09f495b7b
Alex Tran <455406+byalextran@users.noreply.github.com>
2025-12-25 23:15:09 -0800

    updated version

--- version.js
+++ version.js
@@ -1,8 +1,14 @@
-const APP_VERSION = '1.0.2';
+const APP_VERSION = '1.1.1';
 
 const CHANGELOG = [
   {
-    version: '1.0.2',
+    version: '1.1.1',
+    changes: [
+      'UI color, size, and label adjustments.'
+    ]
+  },
+  {
+    version: '1.1.0',
     changes: [
       'Entering faans on mobile shows a number pad (instead of a keyboard).',
       "Little optimizations like this make me happy. 😁"
@@ -10,13 +16,13 @@ const CHANGELOG = [
   }, {
     version: '1.0.1',
     changes: [
-      'Added footer with changelog and other links'
+      'Added footer with changelog and other links.'
     ]
   },
   {
     version: '1.0.0',
     changes: [
-      'Initial release'
+      'Initial release.'
     ]
   }
 ];
